ddd3e2f687db776c7fcc6f78a9c8ebbef7e75d9d
Haardik Dharma <hd2585@nyu.edu>
2024-10-13 20:35:14 -0400

    Tests for update and exception handling

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,13 +19,14 @@ TestInventoryModel API Service Test Suite
 """
 
 # pylint: disable=duplicate-code
-from service.models import db, Inventory
 import os
 import logging
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from service.models import db, Inventory
+from service.common import status
 from tests.factories import InventoryFactory
 from wsgi import app
-from service.common import status
 
 
 DATABASE_URI = os.getenv(
@@ -110,8 +111,8 @@ class TestInventoryService(TestCase):
         new_inventory = response.get_json()
         self.assertEqual(new_inventory["name"], test_inventory.name)
         self.assertEqual(new_inventory["quantity"], test_inventory.quantity)
-        self.assertEqual(new_inventory["condition"], test_inventory.condition)
-        self.assertEqual(new_inventory["stock_level"], test_inventory.stock_level)
+        self.assertEqual(new_inventory["condition"], test_inventory.condition.value)
+        self.assertEqual(new_inventory["stock_level"], test_inventory.stock_level.value)
 
         # Check that the location header was correct
         response = self.client.get(location)
@@ -119,8 +120,8 @@ class TestInventoryService(TestCase):
         new_inventory = response.get_json()
         self.assertEqual(new_inventory["name"], test_inventory.name)
         self.assertEqual(new_inventory["quantity"], test_inventory.quantity)
-        self.assertEqual(new_inventory["condition"], test_inventory.condition)
-        self.assertEqual(new_inventory["stock_level"], test_inventory.stock_level)
+        self.assertEqual(new_inventory["condition"], test_inventory.condition.value)
+        self.assertEqual(new_inventory["stock_level"], test_inventory.stock_level.value)
 
     # TEST LIST
     def test_get_inventory_list(self):
@@ -144,15 +145,40 @@ class TestInventoryService(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], created_inventory.name)
         self.assertEqual(data["quantity"], created_inventory.quantity)
-        self.assertEqual(data["condition"], created_inventory.condition)
-        self.assertEqual(data["stock_level"], created_inventory.stock_level)
+        self.assertEqual(data["condition"], created_inventory.condition.value)
+        self.assertEqual(data["stock_level"], created_inventory.stock_level.value)
 
     def test_get_inventory_not_found(self):
         """It should not return an inventory that does not exist"""
         response = self.client.get(f"{BASE_URL}/1")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    
-        # TEST DELETE
+
+    # TEST UPDATE
+    def test_update_inventory(self):
+        """It should Update an existing Inventory"""
+        # create a inventory item to update
+        test_inventory = InventoryFactory()
+        response = self.client.post(BASE_URL, json=test_inventory.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the inventory
+        new_inventory = response.get_json()
+        logging.debug(new_inventory)
+        new_inventory["name"] = "updated-name"
+        new_inventory["quantity"] = 100
+        new_inventory["condition"] = "OPENBOX"
+        new_inventory["stock_level"] = "LOW_STOCK"
+        response = self.client.put(
+            f"{BASE_URL}/{new_inventory['id']}", json=new_inventory
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_inventory = response.get_json()
+        self.assertEqual(updated_inventory["name"], "updated-name")
+        self.assertEqual(updated_inventory["quantity"], 100)
+        self.assertEqual(updated_inventory["condition"], "OPENBOX")
+        self.assertEqual(updated_inventory["stock_level"], "LOW_STOCK")
+
+    # TEST DELETE
     def test_delete_inventory(self):
         """It should Delete an inventory item"""
         # First, create an inventory item
@@ -171,3 +197,70 @@ class TestInventoryService(TestCase):
         response = self.client.get(f"{BASE_URL}/{test_inventory.id}")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+
+######################################################################
+#  T E S T   S A D   P A T H S
+######################################################################
+class TestSadPaths(TestCase):
+    """Test REST Exception Handling"""
+
+    def setUp(self):
+        """Runs before each test"""
+        self.client = app.test_client()
+
+    def test_method_not_allowed(self):
+        """It should not allow update without an inventory id"""
+        response = self.client.put(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_create_inventory_no_data(self):
+        """It should not Create a inventory with missing data"""
+        response = self.client.post(BASE_URL, json={})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_inventory_no_content_type(self):
+        """It should not Create a inventory with no content type"""
+        response = self.client.post(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_create_inventory_wrong_content_type(self):
+        """It should not Create a inventory with the wrong content type"""
+        response = self.client.post(BASE_URL, data="hello", content_type="text/html")
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_create_inventory_bad_quantity(self):
+        """It should not Create a inventory with bad quantity data"""
+        test_inventory = InventoryFactory()
+        logging.debug(test_inventory)
+        # quantity should be a number, not a string
+        test_inventory.quantity = "XXX"  # invalid quantity
+        response = self.client.post(BASE_URL, json=test_inventory.serialize())
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # def test_create_inventory_bad_condition(self):
+    #     """It should not Create a inventory with bad condition data"""
+    #     inventory = InventoryFactory()
+    #     logging.debug(inventory)
+    #     # change condition to a bad string
+    #     test_inventory = inventory.serialize()
+    #     test_inventory["condition"] = "XXX"  # invalid condition
+    #     response = self.client.post(BASE_URL, json=test_inventory)
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    ######################################################################
+    #  T E S T   M O C K S
+    ######################################################################
+
+    # @patch("service.routes.Inventory.find_by_name")
+    # def test_bad_request(self, bad_request_mock):
+    #     """It should return a Bad Request error from Find By Name"""
+    #     bad_request_mock.side_effect = DataValidationError()
+    #     response = self.client.get(BASE_URL, query_string="name=fido")
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @patch("service.routes.Inventory.find_by_name")
+    def test_mock_search_data(self, pet_find_mock):
+        """It should showing how to mock data"""
+        pet_find_mock.return_value = [MagicMock(serialize=lambda: {"name": "fido"})]
+        response = self.client.get(BASE_URL, query_string="name=fido")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
